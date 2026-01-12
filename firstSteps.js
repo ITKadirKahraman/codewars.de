7664f1467683c5aca65ab2605fe848767b2d4f9a
Kadir Kahraman <kadirkahraman.it@gmail.com>
2026-01-12 23:40:26 +0100

    Update and rename steps.js to firstSteps.js

--- firstSteps.js
+++ firstSteps.js
@@ -163,3 +163,42 @@ if(4 >= 4 && 8 < 15 && 3 == 3){
 }else{
     console.log("b");
 }
+
+
+// funktionen
+
+let discount = 50;
+let price = 500;
+
+function calculatorPrice() {
+    console.log((price - discount) * 1.19);
+}
+
+calculatorPrice();
+
+// funktion mit parameter
+
+function multiplizieren(a, b, c, d) {
+    if((a < b) && (b > c)){
+        let e = (a*b) + (c*d);
+        console.log(e);
+    }
+}
+
+multiplizieren(22, 33, 24.3, 88.5);
+
+// funktion mit parametern und return
+
+function subtrahieren(zahl, zahl2, zahl3, zahl4) {
+    let value = (zahl * zahl2) - (zahl3 - zahl4);
+    return value;
+    return console.log(value);
+}
+
+function plus(zahl, zahl2, zahl3, zahl4) {
+    let value = (zahl * zahl2) + (zahl3 * zahl4);
+    return console.log(value);
+}
+
+console.log(subtrahieren(13, 33, 12, 10));
+plus(13, 33, 12, 10);
